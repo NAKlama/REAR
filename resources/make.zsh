ebd8469687876fa7a8597599514b23f26e97481b
Frederik Klama <github@fklama.de>
2015-11-30 10:20:43 +0100

    Added batik and resources file

---
+++ resources/make.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+for i in 16 32 64
+do 
+	for f in icons/svg/*
+	do
+		w=${i}
+		if [[ ${f:t:r} = "Arrow_facing_right_-_Green" ]]
+		then
+			w=$((${i}*2))
+		fi
+		inkscape -f ${f} -w ${w} -h ${i} -e icons/${i}/${f:t:r}.png
+		convert icons/${i}/${f:t:r}.png -colorspace Gray icons/${i}/gray/${f:t:r}.png
+	done
+done
+
+zip -r ../resources.zip icons license
