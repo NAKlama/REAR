27dc0584467d448e4c53d005a99613fdb08e283a
Frederik Klama <fklama1@gwdg.de>
2015-12-09 16:55:34 +0100

    Started on SettingsDialog

---
+++ REAR Controller/src/de/uni/goettingen/REARController/GUI/Dialogs/SettingsDialog.java
@@ -0,0 +1,43 @@
+package de.uni.goettingen.REARController.GUI.Dialogs;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class SettingsDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	
+	private JButton okButton;
+	private JButton cancelButton;
+	
+	public SettingsDialog() {
+		setTitle("REAR Controller - Settings");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				okButton = new JButton("OK");
+				okButton.setActionCommand("Settings_OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Settings_Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
