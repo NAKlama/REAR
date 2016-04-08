772d66d6056b050b702c9c921c61446725686fee
Frederik Klama <fklama1@gwdg.de>
2016-04-08 16:17:52 +0200

    Catch casting errrors

--- REAR Controller/src/de/uni/goettingen/REARController/GUI/DataTablePanel.java
+++ REAR Controller/src/de/uni/goettingen/REARController/GUI/DataTablePanel.java
@@ -313,21 +313,30 @@ public class DataTablePanel extends JPanel implements TableModelListener {
 			super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 			String text = "";
 			if(editMode == false) {
-				ClientStatus status = (ClientStatus) value;
-				if(status == null || status.isDisconnected())
-					this.setIcon(disconnIcon);
-				else if(status.isUninitialized())
-					this.setIcon(connIcon);
-				else if(status.getNoMic())
-					this.setIcon(warnIcon);
-				else if(status.getInit())
-					this.setIcon(stoppedIcon);
-				else if(status.getRec())
-					this.setIcon(recIcon);
-				else if(status.getUpload())
-					this.setIcon(uploadIcon);
-				else if(status.getDone())
-					this.setIcon(okIcon);
+				try {
+					ClientStatus status = (ClientStatus) value;
+					if(status == null || status.isDisconnected())
+						this.setIcon(disconnIcon);
+					else if(status.isUninitialized())
+						this.setIcon(connIcon);
+					else if(status.getNoMic())
+						this.setIcon(warnIcon);
+					else if(status.getInit())
+						this.setIcon(stoppedIcon);
+					else if(status.getRec())
+						this.setIcon(recIcon);
+					else if(status.getUpload())
+						this.setIcon(uploadIcon);
+					else if(status.getDone())
+						this.setIcon(okIcon);
+				}
+				catch(ClassCastException e) {
+					try {
+						String status = (String) value;
+						this.setText(status);
+					}
+					catch(ClassCastException e2) {}
+				}
 			}
 			this.setText(text);
 			this.setHorizontalAlignment(DefaultTableCellRenderer.CENTER);
